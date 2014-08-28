83613655387a306b4d8a21d0bb98fb3135c2467f
Matthew Rothenberg <mrothenberg@gmail.com>
2014-08-27 21:39:04 -0400

    implement recursive binary pattern match for Scanner

--- lib/exmoji/scanner.ex
+++ lib/exmoji/scanner.ex
@@ -1,20 +1,37 @@
 defmodule Exmoji.Scanner do
-  # Create the regex matcher for scan as a precompiled module attribute.
-  # In order to do this at compile time we need it to be it's own module.
-
-  fbs_pattern = Exmoji.chars(include_variants: true) |> Enum.join("|")
-  @fbs_regexp Regex.compile!( "(?:#{fbs_pattern})" )
-
   @doc """
   Scans a string for all EmojiChars contained within.
 
   Returns a list of all EmojiChars contained within that string, in order.
   """
   def scan(str) do
-    Regex.scan(@fbs_regexp, str)
-    |> Enum.map(&List.first/1)
+    # rscan(str)
+    bscan(str)
     |> Enum.map(&Exmoji.char_to_unified/1)
     |> Enum.map(&Exmoji.find_by_unified/1)
   end
+
+  # regex scan, returns a list of emoji as char glyphs
+  fbs_pattern = Exmoji.chars(include_variants: true) |> Enum.join("|")
+  @fbs_regexp Regex.compile!( "(?:#{fbs_pattern})" )
+  def rscan(str) do
+    Regex.scan(@fbs_regexp, str)
+    |> Enum.map(&List.first/1)
+  end
+
+  # recursive binary pattern match to do the same as above
+  def bscan(str), do: _bscan(str, [])
+
+  # make guarded functions to pattern match on all known glyphs at binary head.
+  for glyph <- Exmoji.chars(include_variants: true) do
+    defp _bscan(<< unquote(glyph), tail::binary >>, acc) do
+      # if we match, add glyph to head of accumulator list and move on
+      _bscan(tail, [unquote(glyph) | acc])
+    end
+  end
+  # if nothing is found, cut the head off and move on
+  defp _bscan(<< _head::utf8, tail::binary >>, acc), do: _bscan(tail, acc)
+  # when we reach the end, return reversed accumulator
+  defp _bscan(<<>>, acc), do: Enum.reverse(acc)
 
 end
