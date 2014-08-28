cafe5bb6edac45e54055be768bcdbb536388eadc
Matthew Rothenberg <mrothenberg@gmail.com>
2014-08-28 12:58:04 -0400

    some placeholder benchmark/profile stuff
    will probably remove later…

--- mix.exs
+++ mix.exs
@@ -25,6 +25,9 @@ defmodule Exmoji.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    [{:jazz, "~> 0.2.0"}]
+    [
+      {:jazz, "~> 0.2.0"},
+      {:exprof, "~> 0.1.2", only: :dev}
+    ]
   end
 end
