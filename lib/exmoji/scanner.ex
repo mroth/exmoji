91f3ced57dacc85dbe0a608dee31d7512a0b9ff7
Matthew Rothenberg <mrothenberg@gmail.com>
2014-08-28 23:49:03 -0400

    massive cleanup of Scanner
    make sure we presort binary strings

--- lib/exmoji/scanner.ex
+++ lib/exmoji/scanner.ex
@@ -1,37 +1,77 @@
 defmodule Exmoji.Scanner do
+  @moduledoc """
+  Provides fast searches against binary strings for the presence of UTF-8
+  encoded Emoji characters.
+  """
+
   @doc """
   Scans a string for all EmojiChars contained within.
 
   Returns a list of all EmojiChars contained within that string, in order.
   """
   def scan(str) do
-    # rscan(str)
     bscan(str)
     |> Enum.map(&Exmoji.char_to_unified/1)
     |> Enum.map(&Exmoji.from_unified/1)
   end
 
-  # regex scan, returns a list of emoji as char glyphs
+  # DEPRECATED: regex scan, returns a list of emoji as char glyphs
+  #
+  # Regular expressions are incredibly slow in Erlang, so this was replaced with
+  # an efficient binary scan.  It remains here in the code for two reasons:
+  #   a.) historical purposes and learning
+  #   b.) since we still use this regex in the ruby and node versions of this
+  #       library, this allows us to easily test and compare to make sure our
+  #       new algorithm produces identical results.
+  #
+  # Thus it is kept as public so we can compare it in test...
   fbs_pattern = Exmoji.chars(include_variants: true) |> Enum.join("|")
   @fbs_regexp Regex.compile!( "(?:#{fbs_pattern})" )
+  @doc false
   def rscan(str) do
     Regex.scan(@fbs_regexp, str)
     |> Enum.map(&List.first/1)
   end
 
-  # recursive binary pattern match to do the same as above
+  # Binary pattern match scan to do the same as above!
+  #
+  # This looks deceptively simple, but before modifying it, bear in mind that
+  # an EmojiChar rendered glyph can be represented as 1..3 utf8 codepoints in
+  # sequence, and some of the longer sequences contain other sequences as
+  # sub-sequences, so you want to always try to match longer sequences first, or
+  # risk cutting a character in half.
+  #
+  # Essentially the logic here is:
+  #   1. Make functions to pattern match on all known glyphs at binary head. We
+  #      order these functions based on the bitsize of the pattern we will match
+  #      against, to make sure we dont cut a character in half and get the small
+  #      variant.
+  #   2. If the function matches head of pattern (match), add glyph to head of
+  #      our results accumulator and recurse on the remainder of the binary.
+  #   3. If no pattern matches head of binary, chop of one utf8 segment and
+  #      throw it away (non-match), recurse on the remainder of the binary.
+  #   4. If end of the binary is reached, reverse the accumulator and return
+  #      results!
+  #
+  @doc false
   def bscan(str), do: _bscan(str, [])
 
-  # make guarded functions to pattern match on all known glyphs at binary head.
-  for glyph <- Exmoji.chars(include_variants: true) do
+  # first we sort all known char glyphs by reverse bitsize, so we can use the
+  # bigger binary patterns first when defining our pattern match functions.
+  sorted_chars = Enum.sort(
+    Exmoji.chars(include_variants: true),
+    &(&1 > &2)
+  )
+
+  # define functions that pattern match against each emojichar binary at head.
+  for glyph <- sorted_chars do
     defp _bscan(<< unquote(glyph), tail::binary >>, acc) do
-      # if we match, add glyph to head of accumulator list and move on
       _bscan(tail, [unquote(glyph) | acc])
     end
   end
-  # if nothing is found, cut the head off and move on
+  # if nothing is found, cut the head off and move on.
   defp _bscan(<< _head::utf8, tail::binary >>, acc), do: _bscan(tail, acc)
-  # when we reach the end, return reversed accumulator
+  # when we reach the end, return reversed accumulator.
   defp _bscan(<<>>, acc), do: Enum.reverse(acc)
 
 end
--- test/scanner_test.exs
+++ test/scanner_test.exs
@@ -3,42 +3,49 @@ defmodule ScannerTest do
 
   alias Exmoji.Scanner
 
+  @case_exact         "🚀"
+  @case_multi         "\x{0023}\x{FE0F}\x{20E3}"
+  @case_variant       "flying on my 🚀 to visit the 👾 people."
+  @case_multivariant  "first a \x{0023}\x{FE0F}\x{20E3} then a 🚀"
+  @case_duplicates    "flying my 🚀 to visit the 👾 people who have their own 🚀 too."
+  @case_none          "i like turtles"
+
   #
   # #scan
   #
   test ".scan - should find the proper EmojiChar from a single string char" do
-    results = Scanner.scan("🚀")
+    results = Scanner.scan(@case_exact)
     assert Enum.count(results) == 1
     assert Enum.at(results,0).__struct__ == Exmoji.EmojiChar
     assert Enum.at(results,0).name == "ROCKET"
   end
 
   test ".scan - should find the proper EmojiChar from a variant encoded char" do
-    results = Scanner.scan("\x{0023}\x{FE0F}\x{20E3}")
+    results = Scanner.scan(@case_multi)
     assert Enum.count(results) == 1
     assert Enum.at(results,0).name == "HASH KEY"
   end
 
   test ".scan - should match multiple chars from within a string" do
-    results = Scanner.scan("flying on my 🚀 to visit the 👾 people.")
+    results = Scanner.scan(@case_variant)
     assert Enum.count(results) == 2
   end
 
   test ".scan - should return multiple matches in proper order" do
-    results = Scanner.scan("flying on my 🚀 to visit the 👾 people.")
+    results = Scanner.scan(@case_variant)
     assert Enum.at(results,0).name == "ROCKET"
     assert Enum.at(results,1).name == "ALIEN MONSTER"
   end
 
   test ".scan - should return multiple matches in proper order for variants" do
-    results = Scanner.scan("first a \x{0023}\x{FE0F}\x{20E3} then a 🚀")
+    results = Scanner.scan(@case_multivariant)
     assert Enum.count(results) == 2
     assert Enum.at(results,0).name == "HASH KEY"
     assert Enum.at(results,1).name == "ROCKET"
   end
 
   test ".scan - should return multiple matches including duplicates" do
-    results = Scanner.scan("flying my 🚀 to visit the 👾 people who have their own 🚀 too.")
+    results = Scanner.scan(@case_duplicates)
     assert Enum.count(results) == 3
     assert Enum.at(results,0).name == "ROCKET"
     assert Enum.at(results,1).name == "ALIEN MONSTER"
@@ -46,7 +53,13 @@ defmodule ScannerTest do
   end
 
   test ".scan - returns and empty list if nothing is found" do
-    assert Scanner.scan("i like turtles") == []
+    assert Scanner.scan(@case_none) == []
+  end
+
+  test ".scan - make sure binary scan gets same results as legacy regex scan" do
+    testcases = [@case_exact, @case_multi, @case_variant, @case_multivariant,
+                 @case_duplicates, @case_none]
+    for tc <- testcases, do: assert Scanner.bscan(tc) == Scanner.rscan(tc)
   end
 
 end
